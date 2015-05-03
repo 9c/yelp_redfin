df225a0395cf91616e5dc8a914824cc5103e3a54
Ken Lu <ken@seeice.com>
2015-05-03 07:50:37 -0400

    added zillow api support

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -3,3 +3,7 @@ $client = Yelp::Client.new({ consumer_key: ENV['YelpConsumerKey'],
                             token: ENV['YelpToken'],
                             token_secret: ENV['YelpTokenSecret']
                           })
+#response = Rubillow::PropertyDetails.deep_search_results({ :address => '2114 Bigelow Ave', :citystatezip => 'Seattle, WA' })
+Rubillow.configure do |configuration|
+  configuration.zwsid = ENV['ZillowSID']
+end
